571b9b78946e302f6546b2cd4365c8659e197c2b
julianalvarezkusy <60948729+julianalvarezkusy@users.noreply.github.com>
2020-04-29 23:03:04 -0300

    Initial commit

---
+++ Sala de Ensayo/Sala de Ensayo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sala_de_Ensayo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
